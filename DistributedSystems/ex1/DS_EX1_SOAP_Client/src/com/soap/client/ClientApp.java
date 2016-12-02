d78afc21085f148b4b1949437addbaba81a4ba9e
Ron Gatenio <ron.gatenio@gmail.com>
2016-12-03 01:45:45 +0200

    now the client's output is written to a log file

--- DistributedSystems/ex1/DS_EX1_SOAP_Client/src/com/soap/client/ClientApp.java
+++ DistributedSystems/ex1/DS_EX1_SOAP_Client/src/com/soap/client/ClientApp.java
@@ -1,5 +1,10 @@
 package com.soap.client;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.rmi.RemoteException;
 import java.util.Scanner;
 
@@ -11,35 +16,47 @@ public class ClientApp {
 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
+		PrintWriter writer = null;
+		String output = "";
+		
 		try {
+			writer = new PrintWriter(new FileWriter("ds-ex1-log.txt"));
 			MainDB mainDB = new MainDBServiceLocator().getMainDBPort();
-			
-			String output = null;
 			
 			System.out.print(">> ");
 			String userInput = reader.nextLine();
+			
 			while (!"quit".equals(userInput)) {
 				try {
 					output = Parser.parseCmd(userInput, mainDB);
 					if (output == null) {
 						output = "## Invalid input";
-					} 
-					System.out.println(output);
+					}
 				} catch (DatabaseException e) {
-					System.out.println("## From Server: " + e.getMessage1());
+					output = "## From Server: " + e.getMessage1();
 				} catch (RemoteException e) {
-					System.out.println("## RemoteException: " + e.getMessage());
-				} 
+					output = "## RemoteException: " + e.getMessage();
+				}
+				System.out.println(output);
+				writer.println(output);
 				
 				System.out.print(">> ");
 				userInput = reader.nextLine();
 			}
 			System.out.println("## Bye");
 			
+		} catch (IOException e) {
+			System.out.println("## Can't write to log file: " + e.getMessage());
 		} catch (ServiceException e) {
-			System.out.println("## ServiceException: " + e.getMessage());
+			output = "## ServiceException: " + e.getMessage();
+			System.out.println(output);
+			writer.println(output);
 		} finally {
 			reader.close();
+			if (writer != null) {
+				writer.flush();
+				writer.close();
+			}
 		}
 	}
 
