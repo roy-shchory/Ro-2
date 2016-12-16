c139f42ecbafe67d4368a23e1008c708308454b1
Ron Gatenio <ron.gatenio@gmail.com>
2016-12-16 15:14:05 +0200

    FIX 1

--- DatabaseSystems/ex2/wet.c
+++ DatabaseSystems/ex2/wet.c
@@ -33,28 +33,31 @@ bool isEmpty(PGresult *res) {
     return PQntuples(res) == 0;
 }
 
-#define MAKE_AND_TEST_RES(res, cmd_in) do { if(!isOk(res = PQexec(conn, cmd_in))) { \
+#define EXECUTE(res, cmd_in) do { if(!isOk(res = PQexec(conn, cmd_in))) { \
                     PQclear(res); \
                     return NULL; }} while(0)
 
-#define TEST_AND_CLEAR_RES(res, cmd_in) do { MAKE_AND_TEST_RES(res, cmd_in); PQclear(res); } while(0)
+#define EXECUTE_AND_CLEAR(res, cmd_in) do { EXECUTE(res, cmd_in); PQclear(res); } while(0)
 
-#define TEST_EMPTY_NOT_EMPTY_AUX(res, cmd_in, errorMsg, toggle) do{ \
-                        MAKE_AND_TEST_RES(res, cmd_in); \
-                        if(isEmpty(res) ^ toggle) { \
-                            printf(errorMsg); \
-                            PQclear(res); \
-                            return NULL; } \
-                        PQclear(res); } while(0)
+#define _EXECUTE_AND_ASSERT_AND_CLEAR_AUX(res, cmd_in, errorMsg, toggle)    do{ \
+                                                                                EXECUTE(res, cmd_in); \
+                                                                                if(isEmpty(res) ^ toggle) { \
+                                                                                    printf(errorMsg); \
+                                                                                    PQclear(res); \
+                                                                                    return NULL; } \
+                                                                                PQclear(res); \
+                                                                            } while(0)
 
-#define TEST_NOT_EMPTY(res, cmd_in, errorMsg) TEST_EMPTY_NOT_EMPTY_AUX(res, cmd_in, errorMsg, false)
+#define EXECUTE_AND_ASSERT_NOT_EMPTY_AND_CLEAR(res, cmd_in, errorMsg) _EXECUTE_AND_ASSERT_AND_CLEAR_AUX(res, cmd_in, errorMsg, false)
 
-#define TEST_EMPTY(res, cmd_in, errorMsg) TEST_EMPTY_NOT_EMPTY_AUX(res, cmd_in, errorMsg, true)
+#define EXECUTE_AND_ASSERT_EMPTY_AND_CLEAR(res, cmd_in, errorMsg) _EXECUTE_AND_ASSERT_AND_CLEAR_AUX(res, cmd_in, errorMsg, true)
 
-#define TEST_ID_EXIST(res, cmd, id) do{ \
-                        sprintf(cmd, "SELECT * FROM Users WHERE ID = %d", id); \
-                        TEST_NOT_EMPTY(res, cmd, ILL_PARAMS);\
-                        } while(0)
+#define ASSERT_ID_EXIST(id) do{ \
+                                PGresult *_abcd_res; \
+                                char _abcd_cmd[250]; \
+                                sprintf(_abcd_cmd, "SELECT * FROM Users WHERE ID = %d", id); \
+                                EXECUTE_AND_ASSERT_NOT_EMPTY_AND_CLEAR(res, _abcd_cmd, ILL_PARAMS);\
+                            } while(0)
 
 // The functions you have to implement
 void* addUser(char* Name, int Age) {
@@ -65,7 +68,7 @@ void* addUser(char* Name, int Age) {
 
     // part 1 - find out if the Users table is empty
     sprintf(cmd, "SELECT * FROM Users");
-    MAKE_AND_TEST_RES(res, cmd);
+    EXECUTE(res, cmd);
 
     if(isEmpty(res)) {
         // if Users is empty, the new ID will be 1
@@ -89,7 +92,7 @@ void* addUser(char* Name, int Age) {
         sprintf(cmd, newID_query);
         PQclear(res); // clear result
 
-        MAKE_AND_TEST_RES(res, newID_query);
+        EXECUTE(res, newID_query);
         newID = atoi(PQgetvalue(res, 0, 0));
     }
 
@@ -97,7 +100,7 @@ void* addUser(char* Name, int Age) {
 
     sprintf(cmd, "INSERT INTO Users VALUES(%s, '%s', %d)", newID_query, Name, Age);
         // note that we didn't use the previous queries result. We used the sub-query as is
-    MAKE_AND_TEST_RES(res, cmd);
+    EXECUTE(res, cmd);
 
     printf(ADD_USER, Name, Age);
     printf(ADD_USER_SUCCESS, newID, Name, Age);
@@ -111,13 +114,13 @@ void* removeUser(int ID) {
 
     printf(REMOVE_USER, ID);
 
-    TEST_ID_EXIST(res, cmd, ID);
+    ASSERT_ID_EXIST(ID);
 
     sprintf(cmd, "DELETE FROM Users WHERE ID = %d", ID);
-    TEST_AND_CLEAR_RES(res, cmd);
+    EXECUTE_AND_CLEAR(res, cmd);
 
     sprintf(cmd, "DELETE FROM Follows WHERE ID1 = %d OR ID2 = %d", ID, ID);
-    TEST_AND_CLEAR_RES(res, cmd);
+    EXECUTE_AND_CLEAR(res, cmd);
 
     return NULL;
 }
@@ -127,14 +130,14 @@ void* follow(int ID1, int ID2) {
 
     printf(FOLLOW, ID1, ID2);
 
-    TEST_ID_EXIST(res, cmd, ID1);
-    TEST_ID_EXIST(res, cmd, ID2);
+    ASSERT_ID_EXIST(ID1);
+    ASSERT_ID_EXIST(ID2);
 
     sprintf(cmd, "SELECT * FROM Follows WHERE ID1 = %d AND ID2 = %d", ID1, ID2);
-    TEST_EMPTY(res, cmd, NOT_APPLICABLE);
+    EXECUTE_AND_ASSERT_EMPTY_AND_CLEAR(res, cmd, NOT_APPLICABLE);
 
     sprintf(cmd, "INSERT INTO Follows VALUES(%d, %d)", ID1, ID2);
-    TEST_AND_CLEAR_RES(res, cmd);
+    EXECUTE_AND_CLEAR(res, cmd);
 
     printf(SUCCESSFUL);
     return NULL;
@@ -145,14 +148,14 @@ void* unfollow(int ID1, int ID2) {
 
     printf(UNFOLLOW, ID1, ID2);
 
-    TEST_ID_EXIST(res, cmd, ID1);
-    TEST_ID_EXIST(res, cmd, ID2);
+    ASSERT_ID_EXIST(ID1);
+    ASSERT_ID_EXIST(ID2);
 
     sprintf(cmd, "SELECT * FROM Follows WHERE ID1 = %d AND ID2 = %d", ID1, ID2);
-    TEST_NOT_EMPTY(res, cmd, NOT_APPLICABLE);
+    EXECUTE_AND_ASSERT_NOT_EMPTY_AND_CLEAR(res, cmd, NOT_APPLICABLE);
 
     sprintf(cmd, "DELETE FROM Follows WHERE ID1 = %d AND ID2 = %d", ID1, ID2);
-    TEST_AND_CLEAR_RES(res, cmd);
+    EXECUTE_AND_CLEAR(res, cmd);
 
     printf(SUCCESSFUL);
     return NULL;
