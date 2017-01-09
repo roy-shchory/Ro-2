77a72a8818228de4733b52965021f0033da61a4f
Ron Gatenio <ron.gatenio@gmail.com>
2017-01-09 21:57:09 +0200

    finished Q1 code

--- DistributedSystems/ex3/DS_EX3_Q1/zook/src/main/java/com/zook/zook/ZkConnector.java
+++ DistributedSystems/ex3/DS_EX3_Q1/zook/src/main/java/com/zook/zook/ZkConnector.java
@@ -9,7 +9,6 @@ import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.Watcher.Event.KeeperState;
-import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.ZooKeeper.States;
 
